5c2e2a57960c5f299818b663fca32e90544b001a
nkpythondeveloper <pyrpadeveloper@gmail.com>
2025-09-21 13:47:24 +0530

    Reverse String Added

---
+++ reverse.py
@@ -0,0 +1,13 @@
+# To reverse a string
+
+def reverse_string(given_str):
+    return given_str[::-1]
+
+if __name__ == "__main__":
+    given_string = "Hello World!"
+    print(reverse_string(given_string))
+
+"""
+Output
+>>> !dlroW olleH
+"""
